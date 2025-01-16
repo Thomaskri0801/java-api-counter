4c4395272f417d443f72c6af8d067adbea7f6cac
Thomas Kristiansen <thomaskri0801@hotmail.com>
2025-01-16 11:23:03 +0100

    Extensions complete

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,16 +1,21 @@
 package com.booleanuk.api.counter;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/counter")
 public class CounterController {
     private int counter;
+    private HashMap<String, Integer> counterName;
 
     public CounterController() {
         this.counter = 0;
+        this.counterName = new HashMap<>();
     }
 
     @GetMapping
@@ -28,6 +33,42 @@ public class CounterController {
     public int decrementCounter() {
         this.counter = counter - 1;
         return counter;
+    }
+
+    @GetMapping("/custom/{name}")
+    public int nameCounter(@PathVariable String name) {
+        if(counterName.containsKey(name)) {
+            return counterName.get(name);
+        } else {
+            counterName.put(name, 0);
+            return counterName.get(name);
+        }
+    }
+
+    @GetMapping("/custom/{name}/increment")
+    public int nameCounterIncrement(@PathVariable String name) {
+        if (counterName.containsKey(name)) {
+            int counterTemp = counterName.get(name);
+            counterTemp = counterTemp + 1;
+            counterName.put(name, counterTemp);
+            return counterName.get(name);
+        } else {
+            counterName.put(name, 0);
+            return counterName.get(name);
+        }
+    }
+
+    @GetMapping("/custom/{name}/decrement")
+    public int nameCounterDecrement(@PathVariable String name) {
+        if (counterName.containsKey(name)) {
+            int counterTemp = counterName.get(name);
+            counterTemp = counterTemp - 1;
+            counterName.put(name, counterTemp);
+            return counterName.get(name);
+        } else {
+            counterName.put(name, 0);
+            return counterName.get(name);
+        }
     }
 
 }
